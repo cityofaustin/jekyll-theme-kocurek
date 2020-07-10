0682b682e16edcd7b2f2359db65304b412d8be65
Matt Langan <mattlangan@users.noreply.github.com>
2020-07-10 14:17:49 -0500

    Incorporate response navigation menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
+
+
+  $('button.hamburger').click(function () {
+    event.preventDefault();
+    $(this).toggleClass('hamburger--collapse is-active');
+    $('.nav-list').toggleClass('expanded');
+    if ($('.nav-list').hasClass('expanded')) {
+      $('.nav-list').attr('aria-expanded', 'true');
+    } else {
+      $('.nav-list').attr('aria-expanded', 'false');
+    }
+    
+  });
+
+
+
   $('.clear-search-button').click(function() {
     $('#results-container').html('');
     $('#search-input').val('');
